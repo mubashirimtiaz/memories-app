a1e8550c25685bbc6beb0903892f39ac300b877b
mubashirimtiaz <mubashir.imtiaz123@gmail.com>
2021-04-12 17:17:22 +0500

    fixed semi colen issue �

--- src/components/splash_screen/Splash-Screen.component.jsx
+++ src/components/splash_screen/Splash-Screen.component.jsx
@@ -3,7 +3,7 @@ import styles from "./splash-screen.module.css";
 function SplashScreen() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <div className={styles.loader}></div>;
+      <div className={styles.loader}></div>
     </div>
   );
 }
